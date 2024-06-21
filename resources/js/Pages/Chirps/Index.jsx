00e352716915be659f86dbc417f01fe10e21dde1
Jordan <jordan.l.partridge@gmail.com>
2024-06-20 18:58:21 -0700

    chirp added with dark mode

---
+++ resources/js/Pages/Chirps/Index.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
+import InputError from '@/Components/InputError';
+import PrimaryButton from '@/Components/PrimaryButton';
+import { useForm, Head } from '@inertiajs/react';
+
+export default function Index({ auth }) {
+    const { data, setData, post, processing, reset, errors } = useForm({
+        message: '',
+    });
+
+    const submit = (e) => {
+        e.preventDefault();
+        post(route('chirps.store'), { onSuccess: () => alert('chirp would have been created') });
+    };
+
+    return (
+        <AuthenticatedLayout user={auth.user}>
+            <Head title="Chirps" />
+
+            <div className="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8 dark:bg-gray-900">
+                <form onSubmit={submit}>
+                    <textarea
+                        value={data.message}
+                        placeholder="What's on your mind?"
+                        className="block w-full border-gray-300 dark:bg-slate-800 dark:text-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
+                        onChange={e => setData('message', e.target.value)}
+                    ></textarea>
+                    <InputError message={errors.message} className="mt-2" />
+                    <PrimaryButton className="mt-4 dark:bg-sky-900 dark:text-white hover:dark:bg-sky-500" disabled={processing}>Chirp</PrimaryButton>
+                </form>
+            </div>
+        </AuthenticatedLayout>
+    );
+}
